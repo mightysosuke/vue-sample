65e20c7463f8267b484bba64362974fa2ac0f3dd
mightysosuke <sosuke.sin@gmail.com>
2022-03-24 20:48:45 +0900

    設定など

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,8 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <HelloWorld msg="Hello World" />
+  <label class="label">Hello Learning</label>
 </template>
 
 <style>
@@ -17,5 +18,9 @@ import HelloWorld from './components/HelloWorld.vue'
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.label {
+  font-size: 80px;
 }
 </style>
